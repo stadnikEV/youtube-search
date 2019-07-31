40a4b8cf951f6bb0b431c8937982c8c94400e73a
Stadnik <Stadnik@MacBook-Air-Evgenij.local>
2019-07-31 08:06:52 +0300

    feature: add lazy-load

--- src/components/App/Component.jsx
+++ src/components/App/Component.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import regexLangsPath from 'utils/regex-langs-path'
 import LanguageRouter from 'pages/LanguageRouter'
 import en from 'react-intl/locale-data/en'
 import ru from 'react-intl/locale-data/ru'
 import { addLocaleData } from 'react-intl'
-import Main from 'pages/main'
-import NotFound from 'pages/not-found'
 
+const Main = React.lazy(() => import('pages/main'/* webpackChunkName: 'mainPage' */))
+const NotFound = lazy(() => import('pages/not-found'/* webpackChunkName: 'notFoundPage' */))
 
 addLocaleData(en)
 addLocaleData(ru)
 
 const AppComponent = () => (
   <LanguageRouter>
-    <Switch>
-      <Route exact path={`:lang(${regexLangsPath}|)/`} component={Main} />
-      <Route component={NotFound} />
-    </Switch>
+    <Suspense fallback={<div />}>
+      <Switch>
+        <Route exact path={`:lang(${regexLangsPath}|)/`} component={Main} />
+        <Route component={NotFound} />
+      </Switch>
+    </Suspense>
   </LanguageRouter>
 )
 
--- src/pages/LanguageRouter/LanguageRouter.jsx
+++ src/pages/LanguageRouter/LanguageRouter.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import setLanguage from 'store/app/actions'
 import PropTypes from 'prop-types'
-import Langs from 'pages/langs'
 import regexLangsPath from 'utils/regex-langs-path'
 
+const Langs = lazy(() => import('pages/langs'/* webpackChunkName: 'langsPage' */))
 
 const LanguageRouter = (props) => {
   const { children, language } = props
@@ -13,17 +13,19 @@ const LanguageRouter = (props) => {
   const Children = () => children
 
   return (
-    <Switch>
-      <Route exact path="/langs" component={Langs} />
-      {
-        !language && <Redirect to="/langs" />
-      }
-      <Route path={`:lang(${regexLangsPath})`} render={Children} />
-      {
-        language === 'en' && <Route render={Children} />
-      }
-      <Redirect to={`/${language}${window.location.pathname}`} />
-    </Switch>
+    <Suspense fallback={<div />}>
+      <Switch>
+        <Route exact path="/langs" component={Langs} />
+        {
+          !language && <Redirect to="/langs" />
+        }
+        <Route path={`:lang(${regexLangsPath})`} render={Children} />
+        {
+          language === 'en' && <Route render={Children} />
+        }
+        <Redirect to={`/${language}${window.location.pathname}`} />
+      </Switch>
+    </Suspense>
   )
 }
 
