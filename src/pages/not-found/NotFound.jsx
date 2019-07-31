2e2357207fe7907b630def2b8d4d0809b3a91367
Stadnik <Stadnik@MacBook-Air-Evgenij.local>
2019-07-31 09:03:25 +0300

    feature: add page not-found

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import './style.scss'
 
 const NotFound = () => (
-  <div>Not Found</div>
+  <div className="not-found">
+    <div className="not-found__text">
+      404
+    </div>
+  </div>
 )
 
 export default NotFound
